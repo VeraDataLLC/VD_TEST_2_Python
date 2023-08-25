788c7efd518a909921f26f114b5a4007086aad79
Mykyta Heivan <mheivan@veradata.com>
2023-08-25 03:45:43 -0700

    Init files

---
+++ files/__init__.py
@@ -0,0 +1,8 @@
+import os
+
+directory = os.path.split(__file__)[0]
+
+sample2 = os.path.join(directory, 'sample2.csv') # Simple Pipe
+
+sample_flat3 = os.path.join(directory, 'sample_flat3') # Flat file [20,30,5,20,4,2,20]
+result3 = os.path.join(directory, 'result3.csv')
